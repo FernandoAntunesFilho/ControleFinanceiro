d39ef982d8060f6541451f9461c2bf3ccd7c1b02
Fernando Antunes Filho <fernando.antunes1@gmail.com>
2025-06-06 14:49:20 -0300

    Cria a camada Repository para Categoria

---
+++ ControleFinanceiro/Repositories/CategoriaRepository.cs
@@ -0,0 +1,38 @@
+﻿using ControleFinanceiro.Contexts;
+using ControleFinanceiro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleFinanceiro.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly ControleFinanceiroContext _context;
+        public CategoriaRepository(ControleFinanceiroContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Add(Categoria categoria)
+        {
+            await _context.Categorias.AddAsync(categoria);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> Delete(Categoria categoria)
+        {
+            _context.Categorias.Remove(categoria);
+            return await _context.SaveChangesAsync();
+        }
+        
+        public async Task<List<Categoria>> GetAll()
+        {
+            return await _context.Categorias.ToListAsync();
+        }
+
+        public async Task<int> Update(Categoria categoria)
+        {
+            _context.Categorias.Update(categoria);
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ ControleFinanceiro/Repositories/ICategoriaRepository.cs
@@ -0,0 +1,12 @@
+﻿using ControleFinanceiro.Models;
+
+namespace ControleFinanceiro.Repositories
+{
+    public interface ICategoriaRepository
+    {
+        Task<List<Categoria>> GetAll();
+        Task<int> Add(Categoria categoria);
+        Task<int> Update(Categoria categoria);
+        Task<int> Delete(Categoria categoria);
+    }
+}
