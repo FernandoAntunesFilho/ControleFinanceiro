4359af4d86861849d028387510e7ef70a09efacc
Fernando Antunes Filho <fernando.antunes1@gmail.com>
2025-06-06 14:17:47 -0300

    Cria o model Categoria, o contexto da aplicação e a migration.

---
+++ ControleFinanceiro/Contexts/ControleFinanceiroContext.cs
@@ -0,0 +1,12 @@
+﻿using ControleFinanceiro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleFinanceiro.Contexts
+{
+    public class ControleFinanceiroContext : DbContext
+    {
+        public ControleFinanceiroContext(DbContextOptions<ControleFinanceiroContext> options) : base(options) { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
--- ControleFinanceiro/Program.cs
+++ ControleFinanceiro/Program.cs
@@ -1,3 +1,6 @@
+using ControleFinanceiro.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ControleFinanceiroContext>(options => options.UseMySql(connectionString,
+    new MySqlServerVersion(new Version(8, 0, 41))));
 
 var app = builder.Build();
 
