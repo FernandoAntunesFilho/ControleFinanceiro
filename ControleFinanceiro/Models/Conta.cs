747ad8b7e087eee86080919a804b84f1e1c0f7af
Fernando Antunes Filho <fernando.antunes1@gmail.com>
2025-06-09 16:57:02 -0300

    Cria Model Conta

---
+++ ControleFinanceiro/Models/Conta.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleFinanceiro.Models
+{
+    public class Conta
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public decimal ValorInicial { get; set; }
+        public bool Ativo {  get; set; }
+    }
+}
