cb8a3de46c7f9efef661da72eb53a7266587da25
Fernando Antunes Filho <fernando.antunes1@gmail.com>
2025-06-11 15:11:51 -0300

    Cria a camada Service para Conta.

---
+++ ControleFinanceiro/Services/ContaService.cs
@@ -0,0 +1,56 @@
+﻿using ControleFinanceiro.DTOs;
+using ControleFinanceiro.Models;
+using ControleFinanceiro.Repositories;
+
+namespace ControleFinanceiro.Services
+{
+    public class ContaService : IContaService
+    {
+        private readonly IContaRepository _repository;
+        private const string contaNotFoundMessage = "Conta não encontrada.";
+        private const string ContaNameRequiredMessage = "O nome da conta é obrigatório.";
+        public ContaService(IContaRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<int> AddAsync(ContaRequestDTO conta)
+        {
+            if (string.IsNullOrWhiteSpace(conta.Nome)) throw new Exception(ContaNameRequiredMessage);
+
+            var novaConta = new Conta
+            {
+                Nome = conta.Nome,
+                ValorInicial = conta.ValorInicial,
+                Ativo = conta.Ativo,
+            };
+
+            return await _repository.Add(novaConta);
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            var contaExistente = await _repository.GetById(id);
+            if (contaExistente == null) throw new Exception(contaNotFoundMessage);
+
+            return await _repository.Delete(contaExistente);
+        }
+
+        public async Task<List<Conta>> GetAllAsync()
+        {
+            return await _repository.GetAll();
+        }
+
+        public async Task<int> UpdateAsync(Conta conta)
+        {
+            var contaExistente = await _repository.GetById(conta.Id);
+            if (contaExistente == null) throw new Exception(contaNotFoundMessage);
+
+            contaExistente.Nome = conta.Nome;
+            contaExistente.Ativo = conta.Ativo;
+            contaExistente.ValorInicial = conta.ValorInicial;
+
+            return await _repository.Update(contaExistente);
+        }
+    }
+}
---
+++ ControleFinanceiro/Services/IContaService.cs
@@ -0,0 +1,13 @@
+﻿using ControleFinanceiro.DTOs;
+using ControleFinanceiro.Models;
+
+namespace ControleFinanceiro.Services
+{
+    public interface IContaService
+    {
+        Task<List<Conta>> GetAllAsync();
+        Task<int> AddAsync(ContaRequestDTO conta);
+        Task<int> UpdateAsync(Conta conta);
+        Task<int> DeleteAsync(int id);
+    }
+}
